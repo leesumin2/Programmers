3c52caa6a550ce3559692b1b5a0a67434946230a
LEE SuMin <79972431+leesumin2@users.noreply.github.com>
2023-04-24 13:39:36 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,8 +1,3 @@
 function solution(strlist) {
-    var answer = [];
-    var cnt=0;
-    for(let i=0; i<strlist.length; i++){
-        answer.push(strlist[i].length);
-    }
-    return answer;
+    return strlist.map((v)=>v.length)
 }
