ef6b11486378982125715004d601911fcf1bfb07
LEE SuMin <79972431+leesumin2@users.noreply.github.com>
2023-05-02 16:44:36 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,4 +1,12 @@
 function solution(my_string) {
-    var answer = eval(my_string);
-    return answer;
+    var answer = my_string.split(' ');
+    var num = Number(answer[0]);
+    for(let i=0; i<answer.length; i++){
+        if(answer[i]==='+'){
+            num += Number(answer[i+1]);
+        }else if(answer[i]==='-'){
+            num -= Number(answer[i+1]);
+        }
+    }
+    return num;
 }
